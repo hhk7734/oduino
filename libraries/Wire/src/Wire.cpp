6f1f6536a36839e9521c7002b152cdb6435913ca
Hyeonki Hong <hhk7734@gmail.com>
2019-06-20 14:15:21 +0900

    wire basic

---
+++ libraries/Wire/src/Wire.cpp
@@ -0,0 +1,144 @@
+/*
+  Wire.cpp - I2C library for odroid
+
+  Copyright (c) 2019 Hyeonki Hong. All rights reserved.
+  This file is part of the odroid core for Arduino environment.
+ 
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+#include "Wire.h"
+
+#include <stdio.h>
+#include <fcntl.h>
+#include <string.h>
+#include <errno.h>
+#include <linux/i2c.h>
+#include <linux/i2c-dev.h>
+#include <sys/ioctl.h>
+#include <unistd.h>
+
+TwoWire Wire( 0 );
+TwoWire Wire1( 1 );
+
+TwoWire::TwoWire( uint8_t _i2c_num )
+    : i2c_num( _i2c_num )
+{
+}
+
+void TwoWire::begin( void )
+{
+    const char *device = NULL;
+#ifdef ARDUINO_ODROID_N2
+    switch( i2c_num )
+    {
+        case 0: device = "/dev/i2c-2"; break;
+        case 1: device = "/dev/i2c-3"; break;
+    }
+#endif
+    if( ( fd = open( device, O_RDWR ) ) < 0 )
+    {
+        fprintf( stderr, "Unable to open %s : %s\n", device, strerror( errno ) );
+        exit( -1 );
+    }
+}
+
+void TwoWire::end( void ) { close( fd ); }
+
+void TwoWire::setClock( uint32_t clock ) {}
+
+void TwoWire::beginTransmission( uint8_t slave_address )
+{
+    device_address = slave_address;
+    tx_buff_index  = 0;
+}
+
+uint8_t TwoWire::endTransmission( uint8_t stop )
+{
+    if( stop != 0 )
+    {
+        struct i2c_rdwr_ioctl_data i2c;
+        struct i2c_msg             msgs;
+        msgs.flags = 0;
+        msgs.len   = tx_buff_index;
+        msgs.buf   = tx_buff;
+
+        i2c.msgs  = &msgs;
+        i2c.nmsgs = 1;
+
+        return ioctl( fd, I2C_RDWR, &i2c );
+    }
+
+    return 0;
+}
+
+uint8_t TwoWire::requestFrom( uint8_t  slave_address,
+                              uint8_t  len,
+                              uint32_t reg_address,
+                              uint8_t  reg_address_size,
+                              uint8_t  stop )
+{
+    device_address = slave_address;
+    rx_buff_index  = 0;
+    rx_buff_len    = len;
+
+    struct i2c_rdwr_ioctl_data i2c;
+    struct i2c_msg             msgs[2];
+
+    msgs[0].addr  = device_address;
+    msgs[0].flags = 0;    /// write
+    msgs[0].len   = tx_buff_index;
+    msgs[0].buf   = tx_buff;
+
+    msgs[1].addr  = device_address;
+    msgs[1].flags = I2C_M_RD;    /// read
+    msgs[1].len   = rx_buff_len;
+    msgs[1].buf   = rx_buff;
+
+    i2c.msgs  = msgs;
+    i2c.nmsgs = 2;
+
+    return ioctl( fd, I2C_RDWR, &i2c );
+}
+size_t TwoWire::write( uint8_t data )
+{
+    if( tx_buff_index < I2C_BUFFER_SIZE ) { tx_buff[tx_buff_index++] = data; }
+    else
+    {
+    }
+}
+
+size_t TwoWire::write( const uint8_t *data, size_t len )
+{
+    if( tx_buff_index + len - 1 < I2C_BUFFER_SIZE )
+    {
+        memcpy( &tx_buff[tx_buff_index], data, len );
+        tx_buff_index += len;
+    }
+}
+
+int TwoWire::available( void ) { return rx_buff_len - rx_buff_index; }
+
+int TwoWire::read( void )
+{
+    if( rx_buff_index < rx_buff_len ) { return rx_buff[rx_buff_index++]; }
+    else
+    {
+        return 0;
+    }
+}
+
+int  TwoWire::peek( void ) {}
+void TwoWire::flush( void ) {}
--- libraries/Wire/src/Wire.h
+++ libraries/Wire/src/Wire.h
@@ -1,4 +1,94 @@
+/*
+  Wire.cpp - I2C library for odroid
+
+  Copyright (c) 2019 Hyeonki Hong. All rights reserved.
+  This file is part of the odroid core for Arduino environment.
+ 
+  This library is free software; you can redistribute it and/or
+  modify it under the terms of the GNU Lesser General Public
+  License as published by the Free Software Foundation; either
+  version 2.1 of the License, or (at your option) any later version.
+
+  This library is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+  Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public
+  License along with this library; if not, write to the Free Software
+  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
 #ifndef _WIRE_H_
 #define _WIRE_H_
 
-#endif // _WIRE_H_
+#include <stdint.h>
+#include <linux/i2c-dev.h>
+
+#include "Stream.h"
+
+const int I2C_BUFFER_SIZE = I2C_RDWR_IOCTL_MAX_MSGS;
+
+class TwoWire : public Stream
+{
+public:
+    TwoWire( uint8_t _i2c_num );
+    void begin( void );
+    void end( void );
+    void setClock( uint32_t clock );
+    void beginTransmission( uint8_t slave_address );
+    void beginTransmission( int slave_address ) { beginTransmission( ( uint8_t )slave_address ); }
+    uint8_t endTransmission( uint8_t stop );
+    uint8_t endTransmission( void ) { return endTransmission( true ); }
+    uint8_t requestFrom( uint8_t slave_address, uint8_t len )
+    {
+        return requestFrom( slave_address, len, ( uint8_t ) true );
+    }
+    uint8_t requestFrom( uint8_t slave_address, uint8_t len, uint8_t stop )
+    {
+        return requestFrom( slave_address, len, 0, 0, stop );
+    }
+    uint8_t requestFrom( uint8_t  slave_address,
+                         uint8_t  len,
+                         uint32_t reg_address,
+                         uint8_t  reg_address_size,
+                         uint8_t  stop );
+    uint8_t requestFrom( int slave_address, int len )
+    {
+        return requestFrom( ( uint8_t )slave_address, ( uint8_t )len );
+    }
+    uint8_t requestFrom( int slave_address, int len, int stop )
+    {
+        return requestFrom( ( uint8_t )slave_address, ( uint8_t )len, ( uint8_t )stop );
+    }
+
+    virtual size_t write( uint8_t data );
+    virtual size_t write( const uint8_t *data, size_t len );
+    virtual int    available( void );
+    virtual int    read( void );
+    virtual int    peek( void );
+    virtual void   flush( void );
+
+    inline size_t write( unsigned long n ) { return write( ( uint8_t )n ); }
+    inline size_t write( long n ) { return write( ( uint8_t )n ); }
+    inline size_t write( unsigned int n ) { return write( ( uint8_t )n ); }
+    inline size_t write( int n ) { return write( ( uint8_t )n ); }
+    using Print::write;
+
+private:
+    int     fd;
+    uint8_t i2c_num;
+    uint8_t device_address;
+
+    uint8_t rx_buff[I2C_BUFFER_SIZE];
+    uint8_t rx_buff_len;
+    uint8_t rx_buff_index;
+
+    uint8_t tx_buff[I2C_BUFFER_SIZE];
+    uint8_t tx_buff_index;
+};
+
+extern TwoWire Wire;
+extern TwoWire Wire1;
+
+#endif    // _WIRE_H_
