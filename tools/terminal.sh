d8438caca3bee87d3a9c192dc2f3a3efb5856f1a
Hyeonki Hong <hhk7734@gmail.com>
2019-06-25 18:21:13 +0900

    background

--- tools/terminal.sh
+++ tools/terminal.sh
@@ -2,7 +2,7 @@
 
 if [ "$(which mate-terminal)" != "" ]
 then
-    nohup mate-terminal -e "sudo $1"
+    nohup mate-terminal -e "sudo $1" &
     echo "sudo $1"
 else
     echo "Unable to find terminal"
