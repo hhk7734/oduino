a573c61882ed19721003f5bc045787bb3250da3b
Hyeonki Hong <hhk7734@gmail.com>
2019-12-27 18:42:03 +0900

    Update install.sh

--- tools/install.sh
+++ tools/install.sh
@@ -9,7 +9,13 @@ if [ "x${SUDO_USER}" = "x" ]; then
     export SUDO_USER=root
 fi
 
+YELLOW_BOLD=$(tput setaf 3 && tput bold)
+NORMAL=$(tput sgr0)
+
 SCRIPT_PATH=$(dirname "$(realpath "$0")")
+
+printf "\n%s\n" "${YELLOW_BOLD}Adding hardkernel/ppa \
+and installing required packages.${NORMAL}"
 
 add-apt-repository -y ppa:hardkernel/ppa &&\
 apt update
@@ -22,8 +28,13 @@ apt install -y build-essential \
     python3 \
     python3-tk
 
-make -C "$SCRIPT_PATH/ttu0uart" &&\
-make -C "$SCRIPT_PATH/ttu0uart" install
+printf "\n%s\n" "${YELLOW_BOLD}Installing tty0uart.${NORMAL}"
+
+make -C "$SCRIPT_PATH/tty0uart" &&\
+make -C "$SCRIPT_PATH/tty0uart" install
+
+printf "\n%s\n" "${YELLOW_BOLD}Adding udev rules \
+and registering $SUDO_USER to spi, i2c group.${NORMAL}"
 
 if ! grep -q spi: /etc/group ; then
     addgroup spi
@@ -35,11 +46,15 @@ fi
 
 install -m 0644 "$SCRIPT_PATH/52-oduino.rules" /etc/udev/rules.d
 
-chown root:spi /dev/spidev*
-chmod 660 /dev/spidev*
-
-chown root:i2c /dev/i2c-*
-chmod 660 /dev/i2c-*
-
 usermod -aG spi $SUDO_USER
 usermod -aG i2c $SUDO_USER
+
+printf "\n%s\n" "${YELLOW_BOLD}Please reboot before use!!!${NORMAL}"
+
+whiptail --yesno "Would you like to reboot now?" 20 60
+BUTTON=$?
+if [ $BUTTON -eq 0 ]; then
+    # Yes
+    sync
+    reboot
+fi
