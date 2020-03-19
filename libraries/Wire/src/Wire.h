8e614c6ec7238e13451603af26f8e493fcd03fe4
Hyeonki Hong <hhk7734@gmail.com>
2020-03-19 14:37:13 +0900

    Refactor TwoWire

--- libraries/Wire/src/Wire.h
+++ libraries/Wire/src/Wire.h
@@ -1,64 +1,74 @@
 /*
-  Wire.cpp - I2C library for odroid
-
-  Copyright (c) 2019 Hyeonki Hong. All rights reserved.
-  This file is part of the odroid core for Arduino environment.
-
-  This library is free software; you can redistribute it and/or
-  modify it under the terms of the GNU Lesser General Public
-  License as published by the Free Software Foundation; either
-  version 2.1 of the License, or (at your option) any later version.
-
-  This library is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-  Lesser General Public License for more details.
-
-  You should have received a copy of the GNU Lesser General Public
-  License along with this library; if not, write to the Free Software
-  Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
-*/
+ * TWI/I2C library for ODROID.
+ *
+ * Copyright (c) 2015 Arduino LLC. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+ *
+ * Modified 2019-2020 by Hyeonki Hong (ODROID support)
+ */
 
 #ifndef _WIRE_H_
 #define _WIRE_H_
 #pragma once
 
 #include <Arduino.h>
-#include <linux/i2c-dev.h>
 
-const int I2C_BUFFER_SIZE = I2C_RDWR_IOCTL_MAX_MSGS;
+#ifdef max
+#undef max
+#endif
+#ifdef min
+#undef min
+#endif
+
+#include <string>
+
+constexpr int MAX_I2C_BUFFER_SIZE = 1 << 7;
 
 class TwoWire: public HardwareI2C {
 public:
-    TwoWire(uint8_t _i2c_num);
+    TwoWire(const std::string &device);
+    TwoWire(uint8_t deviceNum);
+    ~TwoWire();
 
-    void begin(uint8_t slave_address);
-    void begin(void) { begin(0xFF); }
-    void end(void);
+    void begin(uint8_t slaveAddress) override;
+    void begin(void) override { begin(0xFF); }
+    void end(void) override;
 
-    void setClock(uint32_t freq);
+    void setClock(uint32_t clock) override;
 
-    void beginTransmission(uint8_t slave_address);
-    void beginTransmission(int slave_address) {
-        beginTransmission((uint8_t)slave_address);
+    void    beginTransmission(uint8_t slaveAddress) override;
+    uint8_t endTransmission(bool stopBit) override;
+    uint8_t endTransmission(void) override { return endTransmission(true); }
+
+    uint8_t
+            requestFrom(uint8_t slaveAddress, size_t size, bool stopBit) override;
+    uint8_t requestFrom(uint8_t slaveAddress, size_t size) override {
+        return requestFrom(slaveAddress, size, true);
     }
 
-    uint8_t endTransmission(bool stop);
-    uint8_t endTransmission(void) { return endTransmission(true); }
+    void onReceive(void (*)(int)) override;
+    void onRequest(void (*)(void)) override;
 
-    uint8_t requestFrom(uint8_t slave_address, size_t len, bool stop);
-    uint8_t requestFrom(uint8_t slave_address, size_t len);
-    uint8_t requestFrom(int slave_address, int len);
-    uint8_t requestFrom(int slave_address, int len, int stop);
+    virtual size_t write(uint8_t data) override;
+    virtual size_t write(const uint8_t *data, size_t size) override;
 
-    virtual size_t write(uint8_t data);
-    virtual size_t write(const uint8_t *data, size_t len);
-    virtual int    available(void);
-    virtual int    read(void);
-    virtual int    peek(void);
-    virtual void   flush(void);
-    void           onReceive(void (*)(int));
-    void           onRequest(void (*)(void));
+    virtual int  available(void) override;
+    virtual int  read(void) override;
+    virtual int  peek(void) override;
+    virtual void flush(void) override;
 
     inline size_t write(unsigned long n) { return write((uint8_t)n); }
     inline size_t write(long n) { return write((uint8_t)n); }
@@ -67,16 +77,16 @@ public:
     using Print::write;
 
 private:
-    int     fd;
-    uint8_t i2c_num;
-    uint8_t device_address;
+    std::string mDevice;
+    int         mFd;
+    uint8_t     mSlaveAddress;
 
-    uint8_t rx_buff[I2C_BUFFER_SIZE];
-    uint8_t rx_buff_len;
-    uint8_t rx_buff_index;
+    uint8_t mRxBuffer[MAX_I2C_BUFFER_SIZE];
+    size_t  mRxBufferSize;
+    size_t  mRxBufferIndex;
 
-    uint8_t tx_buff[I2C_BUFFER_SIZE];
-    uint8_t tx_buff_index;
+    uint8_t mTxBuffer[MAX_I2C_BUFFER_SIZE];
+    size_t  mTxBufferIndex;
 };
 
 extern TwoWire &Wire;
